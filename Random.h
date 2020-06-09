07aaab31b5bd980328503e8a94fbc9730dfde731
Computeiful <computeiful@protonmail.com>
2020-06-09 17:26:15 +0000

    Cleaned up the uniform function

--- Random.h
+++ Random.h
@@ -8,42 +8,38 @@
 		__uint128_t seed;
 	};
 
-	static uint64_t Random_Consume(union Random *a) {
+	static uint64_t Random(union Random *a) {
 		a->seed *= 0xDA942042E4DD58B5;
-		return (uint64_t) (a->seed >> 64);
+		return (uint64_t) (a->seed >> 64); 
 	}
 
-	static uint64_t Random_Consume_Range(union Random *a, uint64_t max) {
+	static uint64_t Random_Range(union Random *a, uint64_t max) {
 		assert(max > 0);
 		uint64_t n = 0;
 
 		for(;;) { // Better than modulus
-			n = Random_Consume(a);
+			n = Random(a);
 			if(n < UINT64_MAX - (UINT64_MAX % max)) break;
 		}
 
 		return (n % max);
 	}
 
-	static uint64_t Random_Consume_Between(union Random *a, uint64_t min, uint64_t max) {
+	static uint64_t Random_Between(union Random *a, uint64_t min, uint64_t max) {
 		assert(max > min);
-		return Random_Consume_Range(a, max - min) + min;
+		return Random_Range(a, max - min) + min;
 	}
 
-	static double Random_Consume_Uniform(union Random *a) { // Value between 0.0 and 1.0, mean of 0.5
-		union {
-			uint64_t u64;
-			double f64;
-		} map;
-		map.u64 = 0x3FF0000000000000ULL | (Random_Consume(a) >> 12);
-		return (map.f64 - 1.0);
+	static double Random_Uniform(union Random *a) { // Value between 0.0 and 1.0, mean of 0.5
+		const uint64_t map = 0x3FF0000000000000ULL | (Random(a) >> 12);
+		return (*((double *) &map) - 1.0);
 	}
 
-	static double Random_Consume_Gaussian(union Random *a) { // Normal value, mean of 0.0, s.d. of 1.0 (values cannot exceed 6.0 or -6.0)
+	static double Random_Gaussian(union Random *a) { // Normal value, mean of 0.0, s.d. of 1.0 (values cannot exceed 6.0 or -6.0)
 		double n = 0.0;
 
 		for(char i = 0; i < 12; i++) {
-			n += Random_Consume_Uniform(a);
+			n += Random_Uniform(a);
 		}
 		n -= 6.0;
 
