2bdbb30e681e6f8281e382c0a058556a307b37d3
rorybglover <rorybglover@protonmail.com>
2020-10-28 18:49:52 +0000

    Fix Main.c and move code to a .c file

--- Main.c
+++ Main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 
-#include "Random.h"
+#include "Lean.h"
 
 /*
 This is free and unencumbered software released into the public domain.
@@ -30,37 +30,60 @@ OTHER DEALINGS IN THE SOFTWARE.
 For more information, please refer to <http://unlicense.org/>
 */
 
-void check(union Random *a) { 
+void check(union Lean *a) { 
 	// Check the header for the expected values and bounds of each call
 
 	for(uint64_t i = 0; i < 1000; i++) {
-		const uint64_t n = Random_Range(a, i + 1);
+		const uint64_t n = Lean_Range(a, i + 1);
 		assert(n <= i + 1);
 	}
 
-	for(int i = 0; i < 1000; i++) {
-		uint64_t n = Random_Between(a, 10, 1000);
+	for(int i = 0; i < 10000; i++) {
+		uint64_t n = Lean_Between(a, 10, 1000);
 		assert(n >= 10 && n < 1000);
 	}
 
 	for(int i = 0; i < 1000; i++) {
-		double n = Random_Uniform(a);
+		double n = Lean_Uniform(a);
 		assert(n >= 0 && n <= 1);
 	}
 
 	for(int i = 0; i < 1000; i++) {
-		double n = Random_Gaussian(a);
+		double n = Lean_Gaussian(a);
 		assert(n >= -6 && n <= 6);
 	}
+
+	// Random array test
+	uint8_t temp[15];
+	const int size = sizeof(temp);
+
+	for(size_t i = 0; i < size; i++) {
+		temp[i] = 0;
+	}
+	
+	Lean_Copy(a, temp, size);
+	uint8_t freq = 0;
+
+	for(size_t i = 0; i < size; i++) {
+		if(temp[i] == 0x00) {
+			freq++;
+		}
+	}
+
+	assert(freq <= 1); // More than 1 zero could happen, but would be very rare 
 }
 
 int main(void) {
-	union Random a;
-	a.seed = (__uint128_t) time(NULL); 
-	Random(&a); // Needed when the seed has a low value (such as above)
+	union Lean a;
+	a.seed = 1; 
+	Lean(&a); // Needed when the seed has a low value (such as above)
+	assert(Lean(&a) == 0xBAA09CA73F3265B4); // Correctness check
+
+	a.seed = (__uint128_t) time(NULL);
+	Lean(&a);
 
 	for(int i = 0; i < 10; i++) {
-		printf("%08X\n", (int) Random(&a));
+		printf("%08X\n", (int) Lean(&a));
 	}
 
 	check(&a);
