5058416787830463f705469d946e5ff79d19aa9f
Computeiful <computeiful@protonmail.com>
2021-01-26 23:16:04 +0000

    Add Makefile and improve tests

--- Lean.c
+++ Lean.c
@@ -2,10 +2,10 @@
 
 inline uint64_t Lean(union Lean *a) {
     a->seed *= 0xDA942042E4DD58B5;
-    return (uint64_t) (a->seed >> 64); 
+    return (uint64_t) (a->seed >> 64);
 }
 
-uint64_t Lean_Range(union Lean *a, uint64_t max) {
+uint64_t Lean_Range(union Lean *a, const uint64_t max) {
     assert(max > 0);
     uint64_t n = 0;
 
@@ -17,12 +17,12 @@ uint64_t Lean_Range(union Lean *a, uint64_t max) {
     return (n % max);
 }
 
-uint64_t Lean_Between(union Lean *a, uint64_t min, uint64_t max) { 
+uint64_t Lean_Between(union Lean *a, const uint64_t min, const uint64_t max) { 
     assert(max > min);
     return Lean_Range(a, max - min) + min;
 }
 
-void Lean_Copy(union Lean *a, void *dst, size_t size) { // Copy random bytes onto the array
+void Lean_Copy(union Lean *a, void *dst, const size_t size) { // Copy random bytes onto the array
     uint64_t *body = (uint64_t *) dst;
 
     for(size_t i = 0; i < (size >> 3); i++) {
@@ -32,12 +32,12 @@ void Lean_Copy(union Lean *a, void *dst, size_t size) { // Copy random bytes ont
     uint8_t *foot = (uint8_t *) body;
     
     for(size_t i = 0; i < (size & 0x07); i++) {
-        *foot++ = Lean(a);
+        *foot++ = (uint8_t) Lean(a);
     }
 }
 
 double Lean_Uniform(union Lean *a) { // Value between 0.0 and 1.0, mean of 0.5
-    union {
+    static union {
         uint64_t u;
         double f;
     } map;
--- Lean.h
+++ Lean.h
@@ -13,11 +13,11 @@
 		__uint128_t seed;
 	};
 
-	uint64_t Lean(union Lean *);
-	uint64_t Lean_Range(union Lean *, uint64_t max);
-	uint64_t Lean_Between(union Lean *, uint64_t min, uint64_t max);
-	void Lean_Copy(union Lean *, void *, size_t size); // Copy random bytes onto the array
+	uint64_t Lean(union Lean *); // Random 8-byte value
+	uint64_t Lean_Range(union Lean *, const uint64_t max); // Value between zero and the upper bound
+	uint64_t Lean_Between(union Lean *, const uint64_t min, const uint64_t max); // Value between the lower and upper bound 
+	void Lean_Copy(union Lean *, void *, const size_t size); // Copy random bytes onto the array
 	double Lean_Uniform(union Lean *); // Value between 0.0 and 1.0, mean of 0.5
-	double Lean_Gaussian(union Lean *); // Normal value, mean of 0.0, s.d. of 1.0 (values cannot exceed 6.0 or -6.0)
+	double Lean_Gaussian(union Lean *); // Normal value, mean of 0.0, s.d. of 1.0 (values cannot exceed +6.0 or -6.0, these are hyper-rare anyway)
 
 #endif
